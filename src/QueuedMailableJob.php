b5b1dce89b92c3c5f61ba445feb85a8b634043b8
daxiong <babyseeme@qq.com>
2025-02-11 00:45:09 +0800

    feat(mail): 添加邮件发送的分布式锁机制
    - 在 QueuedMailableJob 类中集成 WiseLocksmith 分布式锁
    - 使用 Redis 锁定机制避免并发发送邮件
    - 捕获 MutexException 和 Throwable 异常，确保邮件发送的可靠性
    - 增加 puv dongping/hyperf-wise-locksmith 依赖

--- src/QueuedMailableJob.php
+++ src/QueuedMailableJob.php
@@ -8,22 +8,34 @@ declare(strict_types=1);
  * @contact  eric@zhu.email
  * @license  https://github.com/hyperf3-ext/mail/blob/master/LICENSE
  */
+
 namespace Hyperf3Ext\Mail;
 
 use Hyperf\AsyncQueue\Job;
 use Hyperf\Context\ApplicationContext;
 use Hyperf3Ext\Mail\Contracts\MailableInterface;
 use Hyperf3Ext\Mail\Contracts\MailManagerInterface;
+use Pudongping\HyperfWiseLocksmith\Locker;
+use Pudongping\WiseLocksmith\Exception\MutexException;
 
 class QueuedMailableJob extends Job
 {
 
-    public function __construct(public MailableInterface $mailable)
+    public function __construct(public MailableInterface $mailable,
+                                private Locker           $locker)
     {
     }
 
     public function handle(): void
     {
-        $this->mailable->send(ApplicationContext::getContainer()->get(MailManagerInterface::class));
+        $mailable = $this->mailable;
+        try {
+            $this->locker->redisLock('email_send_lock', function () use ($mailable) {
+                $mailable->send(ApplicationContext::getContainer()->get(MailManagerInterface::class));
+            }, 10);
+        } catch (MutexException|\Throwable $e) {
+            var_dump('=========== Email Throwable =============', $e->getMessage());
+        }
+
     }
 }
